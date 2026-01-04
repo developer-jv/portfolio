f1ba0585489cd01a6f32d287690353892b0ad9de
Javier Valdez <javier.valdez.15v@gmail.com>
2026-01-04 04:45:32 +0000

    Correccion about me

--- src/components/templates/AboutMe.jsx
+++ src/components/templates/AboutMe.jsx
@@ -32,71 +32,72 @@ const AboutMe = () => {
   return (
     <div className="about-me" id="about-me">
       <h3>About me</h3>
-      <div className="about-me__component">
-        <div className="about-me__side-nav-menu">
-          <div
-            onClick={() => changeInfo(1)}
-            className={infoSelected === 1 ? "selected" : ""}
-          >
-            <p>Profile</p>
+      <div className="about-me__layout">
+        <div className="about-me__ilustrations show">
+          <img src={img} alt="" />
+        </div>
+        <div className="about-me__component">
+          <div className="about-me__side-nav-menu">
+            <div
+              onClick={() => changeInfo(1)}
+              className={infoSelected === 1 ? "selected" : ""}
+            >
+              <p>Profile</p>
+            </div>
+            <div
+              onClick={() => changeInfo(2)}
+              className={infoSelected === 2 ? "selected" : ""}
+            >
+              <p>Studies</p>
+            </div>
+            <div
+              onClick={() => changeInfo(3)}
+              className={infoSelected === 3 ? "selected" : ""}
+            >
+              <p>Skills</p>
+            </div>
           </div>
-          <div
-            onClick={() => changeInfo(2)}
-            className={infoSelected === 2 ? "selected" : ""}
-          >
-            <p>Studies</p>
-          </div>
-          <div
-            onClick={() => changeInfo(3)}
-            className={infoSelected === 3 ? "selected" : ""}
-          >
-            <p>Skills</p>
+
+          <div className="about-me__info-container">
+            <div
+              className={`about-me-info ${
+                infoSelected === 1 ? "show" : "noShow"
+              }`}
+            >
+              <p>
+                Software developer. I like to do both frontend and backend in a web
+                application
+              </p>
+              <p>I have to say that I enjoy the backend more.</p>
+              <p>
+                I focus on giving a good UI/UX experience and on the little details.
+              </p>
+              <p>
+                I am passionate about technology and I love discovering new ways to
+                achieve my goals.
+              </p>
+              <p>I am constantly learning. Student at USAC and Platzi 🎓.</p>
+            </div>
+            <div
+              className={`studies ${infoSelected === 2 ? "show" : "noShow"}`}
+            >
+              {studiesLocalData.map((study) => (
+                <StudiesInfo
+                  key={study.title}
+                  img={study.img}
+                  title={study.title}
+                  subtitle={study.subtitle}
+                  href={study.href}
+                />
+              ))}
+            </div>
+            <div className={`skills ${infoSelected === 3 ? "show" : "noShow"}`}>
+              {skillLocalData.map((item) => (
+                <Skill key={item.text} src={item.imageSrc} text={item.text} />
+              ))}
+            </div>
           </div>
         </div>
-
-        <div className="about-me__info-container">
-          <div
-            className={`about-me-info ${
-              infoSelected === 1 ? "show" : "noShow"
-            }`}
-          >
-            <p>
-              Software developer. I like to do both frontend and backend in a web
-              application
-            </p>
-            <p>I have to say that I enjoy the backend more.</p>
-            <p>
-              I focus on giving a good UI/UX experience and on the little details.
-            </p>
-            <p>
-              I am passionate about technology and I love discovering new ways to
-              achieve my goals.
-            </p>
-            <p>I am constantly learning. Student at USAC and Platzi 🎓.</p>
-          </div>
-          <div
-            className={`studies ${infoSelected === 2 ? "show" : "noShow"}`}
-          >
-            {studiesLocalData.map((study) => (
-              <StudiesInfo
-                key={study.title}
-                img={study.img}
-                title={study.title}
-                subtitle={study.subtitle}
-                href={study.href}
-              />
-            ))}
-          </div>
-          <div className={`skills ${infoSelected === 3 ? "show" : "noShow"}`}>
-            {skillLocalData.map((item) => (
-              <Skill key={item.text} src={item.imageSrc} text={item.text} />
-            ))}
-          </div>
-        </div>
-      </div>
-
-      <div className="about-me__ilustrations show">
-        <img src={img} alt="" />
       </div>
     </div>
   );
